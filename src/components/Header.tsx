68e28524526e824cf60f35609515d03678f0b360
Sebastian Rojas <sebastian.rojas.dev@gmail.com>
2024-05-31 15:11:34 -0400

    feat: header nav, mobile nav started

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,14 @@
 import { HeaderLogo } from "./HeaderLogo";
+import { Navigation } from "./Navigation";
 
 export const Header = () => {
     return (
         <header className="bg-gradient-to-b from-blue-700 to-blue-500 px-4 py-8 lg:px-14 pb-36">
             <div className="max-w-screen-2xl mx-auto">
-                <div className="w-full flex items-center justify-center mb-14">
+                <div className="w-full flex items-center justify-between mb-14">
                     <div className="flex items-center lg:gap-x-16">
                         <HeaderLogo />
+                        <Navigation />
                     </div>
                 </div>
             </div>
--- src/components/HeaderLogo.tsx
+++ src/components/HeaderLogo.tsx
@@ -5,6 +5,15 @@ export const HeaderLogo = () => {
     return (
         <Link href="/">
             <div className="items-center hidden lg:flex">
+                <Image
+                    src="/logo.svg"
+                    alt="Logo"
+                    width={28}
+                    height={28}
+                />
+                <p className="font-semibold text-white text-2xl ml-2.5">
+                    Finance-Manager
+                </p>
             </div>
         </Link>
     );
